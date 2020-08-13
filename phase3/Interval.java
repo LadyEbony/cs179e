c269eb32e0942aebfb8004ffb06c3dc15ce9e103
rchan031 <rchan031@ucr.edu>
2020-08-13 12:14:01 -0700

    Add files via upload

---
+++ phase3/Interval.java
@@ -0,0 +1,37 @@
+package typevisitor;
+import syntaxtree.*;
+import visitor.*;
+import java.util.*;
+
+public class Interval {
+	private String intervalString;
+	private int startInterval;
+	private int endInterval;
+
+	public Interval(String s, int a, int b) {
+		intervalString = s;
+		startInterval = a;
+		endInterval = b;
+	}
+
+	public String getString() {
+		return intervalString;
+	}
+
+	public int getStart() {
+		return startInterval;
+	}
+
+	public int getEnd() {
+		return endInterval;
+	}
+
+	public void setStart(int a) {
+		startInterval = a;
+	}
+
+	public void setEnd(int a) {
+		endInterval = a;
+	}
+	
+}
