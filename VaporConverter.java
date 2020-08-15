c360a1ea2149646f3ee5f24fed2f80693b65dff2
rchan031 <rchan031@ucr.edu>
2020-08-14 22:02:23 -0700

    Add files via upload

---
+++ VaporConverter.java
@@ -0,0 +1,21 @@
+import cs132.vapor.ast.*;
+import java.util.*;
+
+class VaporConverter{
+	private String finalString = "";
+
+	public void conversion(VFunction function, Hashtable<String, String> regMap) {
+		VaporVInstr vvi = new VaporVInstr();
+		VInstr[] body = function.body;
+		for(int line = 0; line < body.length; ++line) {
+			VInstr node = body[line];
+			if(node.accept(regMap, vvi) == null) {
+				finalString = finalString + "\n" + node.toString();
+			}
+			else {
+				finalString = finalString + "\n" + node.accept(regMap, vvi);
+			}
+		}
+		System.out.println(finalString);
+	}
+}
